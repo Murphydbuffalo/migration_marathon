0fe0ccb9b1370b1b845d28a3895c86c535045180
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-10 11:47:54 -0400

    Ran through several migrations creating tables and adding/removing columns.  These were for practice so there are a lot of small migrations where a few more comprehensive ones would have been better.

---
+++ app/models/book.rb
@@ -0,0 +1,14 @@
+class Book < ActiveRecord::Base
+  has_many :categorizations
+  has_many :checkouts, through: :categorizations
+
+  # Validations will check your Model objects BEFORE they are sent to the db
+  # Otherwise your null: false contraint will not throw an error until it is
+  # Sent to the DB.  Server side validations prevent that headache.
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :rating, numericality: {
+  	greater_than_or_equal_to: 0,
+  	less_than_or_equal_to: 100
+  }, allow_nil: true
+end
